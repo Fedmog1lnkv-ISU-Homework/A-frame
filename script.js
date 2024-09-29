1a04a74f744ba118525737cb99c837ceb67d6b28
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2024-09-29 20:21:00 +0800

    feat: Add door animation

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-// Функция изменения свечения кристалла при наведении
-document.querySelector('#crystal1').addEventListener('mouseenter', function () {
-    this.querySelector('a-sphere').setAttribute('material', 'emissive-intensity', '1.2');
-  });
-  document.querySelector('#crystal1').addEventListener('mouseleave', function () {
-    this.querySelector('a-sphere').setAttribute('material', 'emissive-intensity', '0.6');
-  });
-  
-  document.querySelector('#crystal2').addEventListener('mouseenter', function () {
-    this.querySelector('a-sphere').setAttribute('material', 'emissive-intensity', '1.2');
-  });
-  document.querySelector('#crystal2').addEventListener('mouseleave', function () {
-    this.querySelector('a-sphere').setAttribute('material', 'emissive-intensity', '0.6');
-  });
-  
+  // door-animation
+  AFRAME.registerComponent('door-animation', {
+    schema: {
+        opened: {type: 'boolean', default: false}
+    },
+    init: function () {
+        const doorWrapper = this.el;
+
+        doorWrapper.addEventListener('click', () => {
+            if (!this.data.opened) {
+                this.rotateDoor(180);
+            } else {
+                this.rotateDoor(90);
+            }
+            this.data.opened = !this.data.opened;
+        });
+    },
+    rotateDoor: function (targetY) {
+        const doorWrapper = this.el;
+        const currentRotation = doorWrapper.getAttribute('rotation');
+        doorWrapper.setAttribute('animation', {
+            property: 'rotation',
+            to: `${currentRotation.x} ${targetY} ${currentRotation.z}`,
+            dur: 500
+        });
+    }
+});
